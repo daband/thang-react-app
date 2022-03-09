28bb4e4596ecb03ec2a6a39f0184b6db96f44783
daband <smoky20001809@gmail.com>
2022-03-09 21:29:18 +0700

    eqqqq

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      <h1> Hello ThangReactJsqweasdd! </h1>
+      <h1> Hello ThangReactJsdd! </h1>
       </header>
     </div>
   );
