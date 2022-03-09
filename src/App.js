8eaa9a0bd0a6e4c64ba7118c573099a40d86585f
daband <43886076+daband@users.noreply.github.com>
2022-03-09 20:15:41 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      <h1>Hello ThangReactJSasdqwe</h1>
+      <h1>Hello ThangReactJSaaaa</h1>
       
       </header>
     </div>
